be0e36d8c2d35a167ccd97b52beb62efdf2abf77
Emanuell Santos <dfmanu06@gmail.com>
2019-08-26 17:03:08 -0300

    Crud de autores completo

--- src/br/com/manell/livraria/bean/AutorBean.java
+++ src/br/com/manell/livraria/bean/AutorBean.java
@@ -1,11 +1,15 @@
 package br.com.manell.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.manell.livraria.dao.DAO;
 import br.com.manell.livraria.modelo.Autor;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -13,14 +17,31 @@ public class AutorBean {
 	public Autor getAutor() {
 		return autor;
 	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
 
 	public String gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
-		
+		if(this.autor.getId() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
+			
 		this.autor = new Autor();
 		
 		return "livro?faces-redirect=true ";
+	}
+	
+	public void remover(Autor autor) {
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
+	public void carregar(Autor autor) {
+		System.out.println("Carregando autor");
+		this.autor = autor;
 	}
 }
