31b0d081f715f498bbd7ff17fcb5384b77227716
Emanuell Santos <dfmanu06@gmail.com>
2019-08-26 14:39:33 -0300

    Alterando livros

--- src/br/com/manell/livraria/bean/LivroBean.java
+++ src/br/com/manell/livraria/bean/LivroBean.java
@@ -51,12 +51,24 @@ public class LivroBean implements Serializable {
 			return;
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		if(this.livro.getId() == null) {
+			new DAO<Livro>(Livro.class).adiciona(this.livro);
+		} else {
+			new DAO<Livro>(Livro.class).atualiza(this.livro);
+		}
+		
+		this.livro = new Livro();
+		
 	}
 	
 	public void remover(Livro livro) {
 		System.out.println("Removendo livro");
 		new DAO<Livro>(Livro.class).remove(livro);
+	}
+	
+	public void carregar(Livro livro) {
+		System.out.println("Carregando livro");
+		this.livro = livro;
 	}
 	
 	public void gravarAutor() {
