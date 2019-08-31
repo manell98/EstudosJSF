9daa3c5e17679cdce803fbd301b9c9d664f46ccb
Emanuell Santos <dfmanu06@gmail.com>
2019-08-31 11:39:24 -0300

    Criando Entity Venda

--- src/br/com/manell/livraria/bean/VendasBean.java
+++ src/br/com/manell/livraria/bean/VendasBean.java
@@ -1,9 +1,7 @@
 package br.com.manell.livraria.bean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -12,8 +10,7 @@ import javax.inject.Named;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
-import br.com.manell.livraria.dao.LivroDao;
-import br.com.manell.livraria.modelo.Livro;
+import br.com.manell.livraria.dao.VendasDao;
 import br.com.manell.livraria.modelo.Venda;
 
 @Named
@@ -22,12 +19,12 @@ public class VendasBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
-	private LivroDao livroDao;
+	private VendasDao vendasDao;
 
 	public BarChartModel getVendasModel() {
 		
         BarChartModel model = new BarChartModel();
-        model.setTitle("Vendas - 2018-2019");
+        model.setTitle("Vendas - 2019");
         model.setShowPointLabels(true);
         model.setLegendPosition("ne");
         model.setAnimate(true);
@@ -35,40 +32,20 @@ public class VendasBean implements Serializable {
         ChartSeries vendaSerie = new ChartSeries();
         vendaSerie.setLabel("2018");
         
-        List<Venda> vendas = getVendas(1234);
+        List<Venda> vendas = getVendas();
         
         for (Venda venda : vendas) {			
         	vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
 		}
         
-        ChartSeries vendaSerie2019 = new ChartSeries();
-        vendaSerie2019.setLabel("2019");
-        
-        vendas = getVendas(4321);
-        
-        for (Venda venda : vendas) {			
-        	vendaSerie2019.set(venda.getLivro().getTitulo(), venda.getQuantidade());
-		}
-        
         model.addSeries(vendaSerie);
-        model.addSeries(vendaSerie2019);
  
         return model;
     }
 	
-	public List<Venda> getVendas(long seed) {
+	public List<Venda> getVendas() {
 		
-		List<Livro> livros = this.livroDao.listaTodos();
-		List<Venda> vendas = new ArrayList<>();
-		
-		Random random = new Random(seed);
-		
-		for (Livro livro : livros) {
-			
-			Integer quantidade = random.nextInt(500);
-			
-			vendas.add(new Venda(livro, quantidade));
-		}
+		List<Venda> vendas = vendasDao.listaTodos();
 		
 		return vendas;	
 	}
